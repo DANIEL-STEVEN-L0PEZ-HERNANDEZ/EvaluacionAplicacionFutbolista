10d34af04f1cf9c68a611106f90d3e4a0616abe1
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-26 09:41:08 -0600

    Se agrego un comentario

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
 public class Main {
+    /*Examine las tres clases presentadas en la imagen y extraiga los elementos
+comunes que se pueden declarar en una superclase. Después, desarrolle la
+estructura de clases implementando los pilares: Abstracción, Polimorfismo,
+Encapsulamiento y Herencia de la Programación Orientada a Objetos en el
+lenguaje Java. */
     
 }
