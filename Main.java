e49ab9d2b6e267d06f2d7bef87b886410e655029
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-26 09:55:01 -0600

    La clase main se elimino y en la clase SuperInicio

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    /*Examine las tres clases presentadas en la imagen y extraiga los elementos
-comunes que se pueden declarar en una superclase. Después, desarrolle la
-estructura de clases implementando los pilares: Abstracción, Polimorfismo,
-Encapsulamiento y Herencia de la Programación Orientada a Objetos en el
-lenguaje Java. */
-    
-}
--- SuperInicio.java
+++ SuperInicio.java
@@ -1,5 +1,11 @@
 public abstract class SuperInicio {
 
+     /*Examine las tres clases presentadas en la imagen y extraiga los elementos
+comunes que se pueden declarar en una superclase. Después, desarrolle la
+estructura de clases implementando los pilares: Abstracción, Polimorfismo,
+Encapsulamiento y Herencia de la Programación Orientada a Objetos en el
+lenguaje Java. */
+
     protected int id;
     protected String nombre;
     protected String apellido;
