853b928b3ecfccf740f8d690308e5e3c1c0f77b9
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-26 10:20:30 -0600

    Se creo la subclase Masajista que se deriva de la clase SuperInicio.

---
+++ Masajista.java
@@ -0,0 +1,31 @@
+public abstract class Masajista extends SuperInicio{
+
+    private String titulacion;
+    private int aniosExperiencia;
+
+    // Constructor vacío
+    public Masajista() {
+        super();
+    }
+    // Constructor con parámetros
+    public Masajista(int id, String nombre, String apellido, int edad, String titulacion, int aniosExperiencia) {
+        super(id, nombre, apellido, edad);
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    // Getters y Setters para los atributos específicos
+    public String getTitulacion() {
+        return titulacion;
+    }
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+}
